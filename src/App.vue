b7abf0481ba047aadddec21ebe4ddd1a4553d2ac
Vivekanand <vivekanand.rao@gmail.com>
2019-12-15 21:49:20 -0800

    Fundamentals and Directives

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a href="#" class="navbar-brand">COURSES TODO</a>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="card mt-3" v-show="!course.completed" v-for="(course, index) in courses" :key="index">
+        <div class="card-body">
+          <h5 class="card-title">{{ course.title }}</h5>
+          <h6 class="card-subtitle text-danger" v-if="course.dueDate">
+            <span v-show="course.dueDate.getTime() < Date.now()">Overdue:</span>
+            {{ course.dueDate }}
+          </h6>
+          <div class="course-controls">
+            <button class="btn btn-sm btn-outline-success" @click.prevent="course.completed = true">Complete</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+  },
+  data() {
+    return {
+      courses: [
+        { title: 'Understanding OAuth with Node.js', dueDate: new Date(2019, 0, 15), completed:false },
+        { title: 'TypeScript: The Big Picture', dueDate: new Date(2019, 0, 15), completed:false },
+        { title: 'Creating Object-oriented TypeScript Code', dueDate: new Date(2020, 0, 15), completed:false },
+        { title: 'JavaScript Promises and Async Programming', dueDate: new Date(2020, 0, 15), completed:false },
+        { title: 'Creating and Using TypeScript Decorators', completed:false }
+      ]
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.card .course-controls{
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 5px;
+  visibility: hidden;
+}
+.card:hover .course-controls{
+  visibility: visible;
 }
 </style>

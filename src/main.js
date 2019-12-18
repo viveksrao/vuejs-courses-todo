678f73fc38ead24eaf1e6d35a4ac0e7cfe62d32c
Vivekanand Rao <vrao@egnyte.com>
2019-12-18 14:38:39 -0800

    import of fontawesome-svg-core icons.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBook,
+  faTrash,
+  faLink,
+  faListUl,
+  faRandom,
+  faUndo,
+  faStar,
+  faEnvelope
+} from "@fortawesome/free-solid-svg-icons";
 import router from './router'
 import storage from './storage';
+
+library.add(faBook, faTrash, faLink, faListUl, faRandom, faUndo, faStar, faEnvelope);
 
 Vue.config.productionTip = false
 Vue.prototype.$courses = storage;
